bbd1e2e4b610ee4952fd8521c418757a69aa16fd
LostMind62 <juniko361@gmail.com>
2021-02-25 18:04:45 -0600

    agregue las imagenes de dados y la funcionalidad del boton para generar numeros random proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados2';
+  
+  dadoizq ='../assets/img/dice1.png';
+  dadoder ='../assets/img/dice4.png';
+   numero1=1;
+   numero2=2;
+
+  dados(): void{
+    this.numero1=Math.round( Math.random()*5)+1;
+    this.numero2=Math.round( Math.random()*5)+1;
+    
+    this.dadoizq='../assets/img/dice' + this.numero1 + '.png';
+    this.dadoizq='../assets/img/dice' + this.numero2 + '.png';
+
+    
+
+  }
+  
+
 }
